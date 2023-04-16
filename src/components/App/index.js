f7b7d396d71c8b1342dba4fe9f6ca6522069e109
cecesouth <xinhong.c.wang@gmail.com>
2023-04-16 18:51:00 +0800

    NotFound page finished

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Gallery from "../Gallery"
-
+import NotFound from "../NotFound";
 function App() {
     return(
         <>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Gallery />} />
+                <Route path="*" element={<NotFound/>} />
             </Routes>
         </BrowserRouter>
             
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,8 @@
+import "./index.css"
+function NotFound (){
+    return(
+        <h1>Opps, the page doesn't esit</h1>
+    )
+
+}
+export default NotFound
